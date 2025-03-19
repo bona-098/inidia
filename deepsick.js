a2ab68f738233567eb3b31c4689f5a96763daf2c
Bona Matanari <61195048+bona-098@users.noreply.github.com>
2025-03-19 16:18:44 +0800

    Update deepsick.js

--- deepsick.js
+++ deepsick.js
@@ -1,102 +1,86 @@
-var supporting = $("<div id='formattachment'>").dxDataGrid({    
-                        dataSource: storewithmodule('attachmentrequest',modelclass,reqid),
-                        allowColumnReordering: true,
-                        allowColumnResizing: true,
-                        columnsAutoWidth: true,
-                        rowAlternationEnabled: true,
-                        wordWrapEnabled: true,
-                        showBorders: true,
-                        filterRow: { visible: false },
-                        filterPanel: { visible: false },
-                        headerFilter: { visible: false },
-                        searchPanel: {
-                            visible: true,
-                            width: 240,
-                            placeholder: 'Search...',
+{
+    itemType: 'group',
+    colSpan: 2,
+    caption: 'Supporting Document',
+    items: [
+        {
+            itemType: 'simple',
+            template: function(data, container) {
+                var supporting = $("<div id='formattachment'>").dxDataGrid({    
+                    dataSource: storewithmodule('attachmentrequest', modelclass, reqid),
+                    allowColumnReordering: true,
+                    allowColumnResizing: true,
+                    columnsAutoWidth: true,
+                    rowAlternationEnabled: true,
+                    wordWrapEnabled: true,
+                    showBorders: true,
+                    filterRow: { visible: false },
+                    filterPanel: { visible: false },
+                    headerFilter: { visible: false },
+                    searchPanel: {
+                        visible: true,
+                        width: 240,
+                        placeholder: 'Search...'
+                    },
+                    editing: {
+                        useIcons: true,
+                        mode: "popup",
+                        allowAdding: ((isMine == 1 || isPIC == 1) && mode == 'view') || (isMine == 1 && (mode == 'edit' || mode == 'add')) || (admin == 1),
+                        allowUpdating: ((isMine == 1 || isPIC == 1) && mode == 'view') || (isMine == 1 && (mode == 'edit' || mode == 'add')) || (admin == 1),
+                        allowDeleting: ((isMine == 1 || isPIC == 1) && mode == 'view') || (isMine == 1 && (mode == 'edit' || mode == 'add')) || (admin == 1)
+                    },
+                    paging: { enabled: true, pageSize: 10 },
+                    columns: [
+                        { 
+                            caption: 'Attachment',
+                            dataField: "path",
+                            allowFiltering: false,
+                            allowSorting: false,
+                            cellTemplate: cellTemplate,
+                            editCellTemplate: editCellTemplate,
+                            validationRules: [{ type: "required" }],
+                            editorOptions: {
+                                accept: "image/*,.pdf,.doc,.docx,.xls,.xlsx",
+                                uploadMode: "useForm",
+                                maxFileSize: 5242880 // 5MB
+                            }
                         },
-                        editing: {
-                            useIcons:true,
-                            mode: "popup",
-                            allowAdding: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
-                            allowUpdating: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
-                            allowDeleting: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
-                        },
-                        paging: { enabled: true, pageSize: 10 },
-                        columns: [
-                            { 
-                                caption: 'Attachment',
-                                dataField: "path",
-                                allowFiltering: false,
-                                allowSorting: false,
-                                cellTemplate: cellTemplate,
-                                editCellTemplate: editCellTemplate,
-                                validationRules: [{ type: "required" }]
-                            },
-                            {
-                                dataField: "remarks"
-                            },
-                        ],
-                        export: {
-                            enabled: false,
-                            fileName: modname,
-                            excelFilterEnabled: true,
-                            allowExportSelectedData: true
-                        },
-                        onInitialized: function(e) {
-                            dataGridAttachment = e.component;
-                        },
-                        onContentReady: function(e){
-                            moveEditColumnToLeft(e.component);
-                        },
-                        onInitNewRow : function(e) {
-                        },
-                        onToolbarPreparing: function(e) {
-                            e.toolbarOptions.items.unshift({						
-                                location: "after",
-                                widget: "dxButton",
-                                options: {
-                                    hint: "Refresh Data",
-                                    icon: "refresh",
-                                    onClick: function() {
-                                        dataGridAttachment.refresh();
-                                    }
-                                }
-                            })
-                        },
-                        onDataErrorOccurred: function(e) {
-                            // Menampilkan pesan kesalahan
-                            console.log("Terjadi kesalahan saat memuat data (2):", e.error.message);
-                    
-                            // Memuat ulang DataGrid
-                            dataGridAttachment.refresh();
+                        {
+                            dataField: "remarks"
                         }
-                    })
-
-                    return supporting;
-
-=========
-    {
-                                    itemType: 'group',
-                                    colSpan: 2,
-                                    caption: 'Supporting Document',
-                                    items: [
-                                        {
-                                            title: 'Employee',
-                                            editorType: 'dxFileUploader',
-                                            editorOptions: {
-                                                dataSource: emplo,
-                                                displayExpr: function (item) {
-                                                    if (!item) return "";
-                                                    const department = departments.find(dept => dept.id === item.department_id);
-                                                    return `${item.FullName} | ${item.SAPID} | ${department ? department.DepartmentName : "Failed"}`;
-                                                },
-                                                valueExpr: 'id',
-                                                value: Array.isArray(appointmentData.employee) ? appointmentData.employee : [],
-                                                showSelectionControls: true,
-                                                applyValueMode: 'useButtons',
-                                                searchEnabled: true,
-                                                onValueChanged: validateBooking
-                                            }
-                                        },                           
-                                    ]
-                                }       
+                    ],
+                    export: {
+                        enabled: false,
+                        fileName: modname,
+                        excelFilterEnabled: true,
+                        allowExportSelectedData: true
+                    },
+                    onInitialized: function(e) {
+                        dataGridAttachment = e.component;
+                    },
+                    onContentReady: function(e) {
+                        moveEditColumnToLeft(e.component);
+                    },
+                    onToolbarPreparing: function(e) {
+                        e.toolbarOptions.items.unshift({
+                            location: "after",
+                            widget: "dxButton",
+                            options: {
+                                hint: "Refresh Data",
+                                icon: "refresh",
+                                onClick: function() {
+                                    dataGridAttachment.refresh();
+                                }
+                            }
+                        });
+                    },
+                    onDataErrorOccurred: function(e) {
+                        console.log("Terjadi kesalahan saat memuat data:", e.error.message);
+                        dataGridAttachment.refresh();
+                    }
+                });
+                $(container).append(supporting);
+            }
+        }
+    ]
+}
