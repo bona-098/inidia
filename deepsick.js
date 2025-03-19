2674495bc3bd6a0e074850ae97ec66f0c8d36af4
Bona Matanari <61195048+bona-098@users.noreply.github.com>
2025-03-19 16:12:42 +0800

    Update deepsick.js

--- deepsick.js
+++ deepsick.js
@@ -1,35 +1,102 @@
-{
-    "itemType": "group",
-    "colSpan": 2,
-    "caption": "Guest Type",
-    "items": [
-        {
-            "itemType": "simple",
-            "editorType": "dxTagBox",
-            "label": { "text": "Employee" },
-            "dataField": "employee",
-            "editorOptions": {
-                "dataSource": "emplo",
-                "displayExpr": "function (item) { if (!item) return ''; const department = departments.find(dept => dept.id === item.department_id); return `${item.FullName} | ${item.SAPID} | ${department ? department.DepartmentName : 'Failed'}`; }",
-                "valueExpr": "id",
-                "value": "Array.isArray(appointmentData.employee) ? appointmentData.employee : []",
-                "showSelectionControls": true,
-                "applyValueMode": "useButtons",
-                "searchEnabled": true,
-                "onValueChanged": "validateBooking"
-            }
-        },
-        {
-            "itemType": "simple",
-            "editorType": "dxFileUploader",
-            "label": { "text": "Employee" },
-            "editorOptions": {
-                "selectButtonText": "Select photo",
-                "labelText": "",
-                "accept": "image/*",
-                "uploadMode": "useForm",
-                "inputAttr": { "aria-label": "Select Photo" }
-            }
-        }
-    ]
-}
+var supporting = $("<div id='formattachment'>").dxDataGrid({    
+                        dataSource: storewithmodule('attachmentrequest',modelclass,reqid),
+                        allowColumnReordering: true,
+                        allowColumnResizing: true,
+                        columnsAutoWidth: true,
+                        rowAlternationEnabled: true,
+                        wordWrapEnabled: true,
+                        showBorders: true,
+                        filterRow: { visible: false },
+                        filterPanel: { visible: false },
+                        headerFilter: { visible: false },
+                        searchPanel: {
+                            visible: true,
+                            width: 240,
+                            placeholder: 'Search...',
+                        },
+                        editing: {
+                            useIcons:true,
+                            mode: "popup",
+                            allowAdding: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
+                            allowUpdating: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
+                            allowDeleting: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
+                        },
+                        paging: { enabled: true, pageSize: 10 },
+                        columns: [
+                            { 
+                                caption: 'Attachment',
+                                dataField: "path",
+                                allowFiltering: false,
+                                allowSorting: false,
+                                cellTemplate: cellTemplate,
+                                editCellTemplate: editCellTemplate,
+                                validationRules: [{ type: "required" }]
+                            },
+                            {
+                                dataField: "remarks"
+                            },
+                        ],
+                        export: {
+                            enabled: false,
+                            fileName: modname,
+                            excelFilterEnabled: true,
+                            allowExportSelectedData: true
+                        },
+                        onInitialized: function(e) {
+                            dataGridAttachment = e.component;
+                        },
+                        onContentReady: function(e){
+                            moveEditColumnToLeft(e.component);
+                        },
+                        onInitNewRow : function(e) {
+                        },
+                        onToolbarPreparing: function(e) {
+                            e.toolbarOptions.items.unshift({						
+                                location: "after",
+                                widget: "dxButton",
+                                options: {
+                                    hint: "Refresh Data",
+                                    icon: "refresh",
+                                    onClick: function() {
+                                        dataGridAttachment.refresh();
+                                    }
+                                }
+                            })
+                        },
+                        onDataErrorOccurred: function(e) {
+                            // Menampilkan pesan kesalahan
+                            console.log("Terjadi kesalahan saat memuat data (2):", e.error.message);
+                    
+                            // Memuat ulang DataGrid
+                            dataGridAttachment.refresh();
+                        }
+                    })
+
+                    return supporting;
+
+=========
+    {
+                                    itemType: 'group',
+                                    colSpan: 2,
+                                    caption: 'Supporting Document',
+                                    items: [
+                                        {
+                                            title: 'Employee',
+                                            editorType: 'dxFileUploader',
+                                            editorOptions: {
+                                                dataSource: emplo,
+                                                displayExpr: function (item) {
+                                                    if (!item) return "";
+                                                    const department = departments.find(dept => dept.id === item.department_id);
+                                                    return `${item.FullName} | ${item.SAPID} | ${department ? department.DepartmentName : "Failed"}`;
+                                                },
+                                                valueExpr: 'id',
+                                                value: Array.isArray(appointmentData.employee) ? appointmentData.employee : [],
+                                                showSelectionControls: true,
+                                                applyValueMode: 'useButtons',
+                                                searchEnabled: true,
+                                                onValueChanged: validateBooking
+                                            }
+                                        },                           
+                                    ]
+                                }       
