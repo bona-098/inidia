3a4e66770a1b88b9a618c465e66eae5e9224f1e7
Bona Matanari <61195048+bona-098@users.noreply.github.com>
2025-03-20 22:19:15 +0800

    Update deepsick.js

--- deepsick.js
+++ deepsick.js
@@ -1,23 +1,20 @@
 SELECT
     request_ghm.id,
-    request_ghm.requestStatus as rs,
+    request_ghm.requestStatus AS rs,
     request_ghm.startDate,
-    tbl_approverListHistory.fullname as Submit_by,
+    tbl_approverListHistory.fullname AS Submit_by,
     tbl_approverListHistory.approvalType,
     tbl_approverListHistory.approvalDate,
     tbl_approverListHistory.module_id,
-    CAST(CASE
-        WHEN DATEDIFF(day, tbl_approverListHistory.approvalDate, request_ghm.startDate) < 0 THEN 0
-        ELSE DATEDIFF(day, tbl_approverListHistory.approvalDate, request_ghm.startDate)
-    END AS VARCHAR) + ' days, ' +
-    CAST(CASE
-        WHEN DATEDIFF(minute, tbl_approverListHistory.approvalDate, request_ghm.startDate) < 0 THEN 0
-        ELSE (DATEDIFF(minute, tbl_approverListHistory.approvalDate, request_ghm.startDate) % 1440) / 60
-    END AS VARCHAR) + ' hours, ' +
-    CAST(CASE
-        WHEN DATEDIFF(minute, tbl_approverListHistory.approvalDate, request_ghm.startDate) < 0 THEN 0
-        ELSE DATEDIFF(minute, tbl_approverListHistory.approvalDate, request_ghm.startDate) % 60
-    END AS VARCHAR) + ' minutes' AS time_left
+    CASE 
+        WHEN DATEDIFF(day, tbl_approverListHistory.approvalDate, request_ghm.startDate) >= 2 
+        THEN '00:00:00'
+        ELSE FORMAT(DATEADD(MINUTE, 
+            CASE 
+                WHEN DATEDIFF(MINUTE, tbl_approverListHistory.approvalDate, request_ghm.startDate) < 0 THEN 0
+                ELSE DATEDIFF(MINUTE, tbl_approverListHistory.approvalDate, request_ghm.startDate)
+            END, '00:00:00'), 'HH:mm:ss')
+    END AS time_left
 FROM request_ghm
 JOIN tbl_approverListHistory ON request_ghm.id = tbl_approverListHistory.req_id
 WHERE tbl_approverListHistory.approvalType = 'submitted'
