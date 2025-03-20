f5182c0e8bcdc9373656968ca5bb67b87450dd61
Bona Matanari <61195048+bona-098@users.noreply.github.com>
2025-03-20 15:56:40 +0800

    Update deepsick.js

--- deepsick.js
+++ deepsick.js
@@ -1,83 +1,117 @@
-datagrid
-var supporting = $("<div id='formattachment'>").dxDataGrid({    
-                        dataSource: storewithmodule('attachmentrequest',modelclass,reqid),
-                        allowColumnReordering: true,
-                        allowColumnResizing: true,
-                        columnsAutoWidth: true,
-                        rowAlternationEnabled: true,
-                        wordWrapEnabled: true,
-                        showBorders: true,
-                        filterRow: { visible: false },
-                        filterPanel: { visible: false },
-                        headerFilter: { visible: false },
-                        searchPanel: {
-                            visible: true,
-                            width: 240,
-                            placeholder: 'Search...',
-                        },
-                        editing: {
-                            useIcons:true,
-                            mode: "popup",
-                            allowAdding: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
-                            allowUpdating: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
-                            allowDeleting: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
-                        },
-                        paging: { enabled: true, pageSize: 10 },
-                        columns: [
-                            { 
-                                caption: 'Attachment',
-                                dataField: "path",
-                                allowFiltering: false,
-                                allowSorting: false,
-                                cellTemplate: cellTemplate,
-                                editCellTemplate: editCellTemplate,
-                                validationRules: [{ type: "required" }]
-                            },
-                            {
-                                dataField: "remarks"
-                            },
-                        ],
-                        export: {
-                            enabled: false,
-                            fileName: modname,
-                            excelFilterEnabled: true,
-                            allowExportSelectedData: true
-                        },
-                        onInitialized: function(e) {
-                            dataGridAttachment = e.component;
-                        },
-                        onContentReady: function(e){
-                            moveEditColumnToLeft(e.component);
-                        },
-                        onInitNewRow : function(e) {
-                        },
-                        onToolbarPreparing: function(e) {
-                            e.toolbarOptions.items.unshift({						
-                                location: "after",
-                                widget: "dxButton",
-                                options: {
-                                    hint: "Refresh Data",
-                                    icon: "refresh",
-                                    onClick: function() {
-                                        dataGridAttachment.refresh();
-                                    }
-                                }
-                            })
-                        },
-                        onDataErrorOccurred: function(e) {
-                            // Menampilkan pesan kesalahan
-                            console.log("Terjadi kesalahan saat memuat data (2):", e.error.message);
-                    
-                            // Memuat ulang DataGrid
-                            dataGridAttachment.refresh();
-                        }
-                    })
-
-                    return supporting;
-schedler
-{
+form.option('items', [                          
+                        {
                             itemType: 'group',
-                            caption: 'Supporting Doc',
+                            colCount: 1,
+                            caption: 'Interests',
+                            items: [
+                                {
+                                    label: { text: 'Code' },
+                                    dataField: 'code',
+                                    editorOptions: {
+                                        readOnly: true,
+                                        value: appointmentData.code || ''
+                                    }
+                                },
+                                {
+                                    label: { text: 'Purpose' },
+                                    editorType: 'dxTextBox',
+                                    dataField: 'text',
+                                    editorOptions: {
+                                        value: appointmentData.text || ''
+                                    },
+                                    validationRules: [{ type: "required", message: 'Purpose is required', }],
+                                },
+                                {
+                                    label: { text: 'Details' },
+                                    editorType: 'dxTextArea',
+                                    dataField: 'description',
+                                    editorOptions: {
+                                        value: appointmentData.description || ''
+                                    },
+                                    validationRules: [{ type: "required", message: 'Details is required', }],
+                                }
+                            ]
+                        },                        
+                        {
+                            itemType: 'group',
+                            colSpan: 2,
+                            caption: 'Guest Type',
+                            items: [
+                                {
+                                    title: 'Employee',
+                                    label: { text: 'Employee' },
+                                    editorType: 'dxTagBox',
+                                    dataField: 'employee',
+                                    editorOptions: {
+                                        dataSource: emplo,
+                                        displayExpr: function (item) {
+                                            if (!item) return "";
+                                            const department = departments.find(dept => dept.id === item.department_id);
+                                            return `${item.FullName} | ${item.SAPID} | ${department ? department.DepartmentName : "Failed"}`;
+                                        },
+                                        valueExpr: 'id',
+                                        value: Array.isArray(appointmentData.employee) ? appointmentData.employee : [],
+                                        showSelectionControls: true,
+                                        applyValueMode: 'useButtons',
+                                        searchEnabled: true,
+                                        onValueChanged: validateBooking
+                                    }
+                                },
+                                {
+                                    title: 'Guest',
+                                    editorType: 'dxTagBox',
+                                    dataField: 'guest',
+                                    editorOptions: {
+                                        dataSource: [],
+                                        value: Array.isArray(appointmentData.guest) ? appointmentData.guest : [],
+                                        acceptCustomValue: true,
+                                        searchEnabled: true,
+                                        showSelectionControls: true,
+                                        applyValueMode: 'useButtons',
+                                        onCustomItemCreating: function (args) {
+                                            let newValue = args.text;
+                                            let guests = form.option('formData').guest || [];
+                                            if (!guests.includes(newValue)) {
+                                                guests.push(newValue);
+                                                args.customItem = newValue;
+                                            } else {
+                                                args.customItem = null;
+                                            }
+                                            form.updateData('guest', guests);
+                                            validateBooking();
+                                        }
+                                    }
+                                },
+                                {
+                                    title: 'Family',
+                                    editorType: 'dxTagBox',
+                                    dataField: 'family',
+                                    editorOptions: {
+                                        dataSource: [],
+                                        value: Array.isArray(appointmentData.family) ? appointmentData.family : [],
+                                        acceptCustomValue: true,
+                                        searchEnabled: true,
+                                        showSelectionControls: true,
+                                        applyValueMode: 'useButtons',
+                                        onCustomItemCreating: function (args) {
+                                            let newValue = args.text;
+                                            let familys = form.option('formData').family || [];
+                                            if (!familys.includes(newValue)) {
+                                                familys.push(newValue);
+                                                args.customItem = newValue;
+                                            } else {
+                                                args.customItem = null;
+                                            }
+                                            form.updateData('family', familys);
+                                            validateBooking();
+                                        }
+                                    }
+                                },
+                            ]
+                        },
+                        {
+                            itemType: 'group',
+                            caption: 'Supporting Document',
                             colSpan: 2,
                             items: [                                
                                 {
@@ -102,9 +136,9 @@ schedler
                                             editing: {
                                                 useIcons:true,
                                                 mode: "popup",
-                                                // allowAdding: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
-                                                // allowUpdating: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
-                                                // allowDeleting: (((isMine == 1 || isPIC == 1) && mode == 'view') ? true : (isMine == 1) && mode == 'edit' || mode == 'add' ) ? true : (admin == 1 ? true : false),
+                                                allowAdding: true,
+                                                allowUpdating: true,
+                                                allowDeleting: true,
                                             },
                                             paging: { enabled: true, pageSize: 10 },
                                             columns: [
@@ -115,7 +149,7 @@ schedler
                                                     allowSorting: false,
                                                     cellTemplate: cellTemplate,
                                                     editCellTemplate: editCellTemplate,
-                                                    validationRules: [{ type: "required" }]
+                                                    validationRules: [{ type: "required", message: 'Details is required', }],
                                                 },
                                                 {
                                                     dataField: "remarks"
@@ -162,4 +196,5 @@ schedler
                                 } 
                                 // : null
                             ].filter(Boolean) // Menghapus item null dari array
-                        }  
+                        }                                         
+                    ]);
