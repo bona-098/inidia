bc50503436f3fbb2db46038d56a33c494d1288c8
Bona Matanari <61195048+bona-098@users.noreply.github.com>
2025-03-21 11:17:24 +0800

    Update deepsick.js

--- deepsick.js
+++ deepsick.js
@@ -1,3 +1,15 @@
+$("#schedulerPopup").dxPopup({
+    onHidden: function () {
+        // Reset file uploader setelah popup ditutup
+        if (dataGridAttachment) {
+            let dataSource = dataGridAttachment.getDataSource();
+            dataSource.store().clear(); // Hapus data di grid
+            dataGridAttachment.refresh(); // Refresh grid
+        }
+    }
+});
+
+
 let fileUploader = $(fileUploaderElement).dxFileUploader({
     multiple: false,
     accept: ".pptx,.ppt,.docx,.pdf,.xlsx,.csv,.png,.jpg,.jpeg,.zip",
@@ -10,6 +22,20 @@ let fileUploader = $(fileUploaderElement).dxFileUploader({
     },
     onUploadError: function(e) {
         DevExpress.ui.notify(e.request.response, "error");
+
+        form.option("onFieldDataChanged", function(e) {
+    if (e.dataField === "guest" || e.dataField === "family") {
+        validateBooking();
+    }
+});
+
+$("#cancelButton").on("click", function() {
+    form.resetValues(); // Reset semua nilai form
+    if (dataGridAttachment) {
+        dataGridAttachment.refresh(); // Pastikan tidak ada file yang tersimpan
+    }
+});
+
     },
     onDisposing: function() {
         fileUploader.option("value", []); // Reset file yang diunggah
